3a5117f01719729bf2e56c27a9d5c72d74ac50d9
tom.jm03 <Lilleboysweetie@gmail.com>
2023-01-03 15:19:54 +0100

    [FEATURE] add clear command

---
+++ cogs/clear.py
@@ -0,0 +1,22 @@
+import discord, asyncio
+from discord import app_commands, Permissions
+from discord.ext import commands
+
+
+
+
+class clear(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @app_commands.default_permissions(manage_messages=True)
+    @app_commands.command(name="clear", description="Clears messages in a channel")
+    async def clear(self, interaction, amount: int = 1):
+        await interaction.response.defer()
+        purger = await interaction.channel.purge(limit=amount, before=interaction.created_at)
+        embed = discord.Embed(title="Cleared messages", description=f"Deleted {len(purger)} messages.", color=0x00ff00)
+        message = await interaction.followup.send(embed=embed)
+        await asyncio.sleep(2)
+        await message.delete()
+async def setup(bot):
+    await bot.add_cog(clear(bot))
