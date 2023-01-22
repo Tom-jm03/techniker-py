9e671e09beeb3d802d2243c33469c088c946c352
tom.jm03 <Lilleboysweetie@gmail.com>
2023-01-22 16:17:48 +0100

    [BUGFIX] fix avatar fetch

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -10,8 +10,7 @@ class avatar(commands.Cog):
     async def avatar(self, interaction: discord.Interaction, user: discord.User = None):
         await interaction.response.defer()
         if user is None:
-            user = self.bot.fetch_user(interaction.user.id)
-            print(user)
+            user = await self.bot.fetch_user(interaction.user.id)
         embed = discord.Embed(title=f"{user.name}'s avatar", color=0x00ff00)
         embed.set_image(url=user.avatar)
         embed.timestamp = interaction.created_at
